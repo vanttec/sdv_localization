d046b6cfa4f41cbc1a7eb80c5343bb5417b39e25
EdisonAltamirano <A00825234@tec.mx>
2022-08-04 14:18:54 -0500

    imu fix

--- imu_sdv/scripts/imu_control.py
+++ imu_sdv/scripts/imu_control.py
@@ -12,7 +12,7 @@ from sensor_msgs.msg import NavSatFix
 class ImuControl:
     def __init__(self) -> None:
         self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
-        self.path_pub = rospy.Publisher("/sdv/path", Path, queue_size=50)
+        self.path_pub = rospy.Publisher("/sdv/path1", Path, queue_size=50)
         self.imu_sub = rospy.Subscriber("/vectornav/Odom", Odometry, self.imu_callback)
         self.imu_sub = rospy.Subscriber("/vectornav/GPS", NavSatFix, self.gps_callback)
         self.odom_broadcaster = tf.TransformBroadcaster()
@@ -27,8 +27,6 @@ class ImuControl:
 
         self.current_time = rospy.Time.now()
         self.last_time = rospy.Time.now()
-
-        self.sdv_path = Path()
         self.past_x=-1
         self.past_y=-1
         self.past_z=-1
@@ -39,7 +37,10 @@ class ImuControl:
         self.current_node=None
         self.origin_x=-100.289790
         self.origin_y=25.651249
-        self.threshold=76923.0769
+        self.offsetx=74824.56666
+        self.offsety=350000
+        self.offset_angle=-(180)*(math.pi/180)
+        #360+20
     def imu_callback(self,data):
         self.vx=data.twist.twist.linear.x
         self.vy=data.twist.twist.linear.y
@@ -52,10 +53,12 @@ class ImuControl:
 
     def imu_odom(self):
         if self.imu_longitude!=0 and self.imu_latitude!=0 and self.enable_imu==False:
-            new_x=(self.imu_longitude-self.origin_x )*self.threshold
-            new_y=(self.imu_latitude-self.origin_y)*self.threshold
-            current_coord=str(new_x)+" "+str(new_y)+" "+"0 0 0 0 map odom"
+            new_x=(self.imu_longitude-self.origin_x )*self.offsetx
+            new_y=(self.imu_latitude-self.origin_y)*self.offsety
+            current_coord=str(new_x)+" "+str(new_y)+" "+"0 "+str(self.offset_angle)+" 0 0 map odom2"
             rospy.loginfo("New coord")
+            rospy.loginfo(self.imu_longitude-self.origin_x )
+            rospy.loginfo(self.imu_latitude-self.origin_y)
             rospy.loginfo(current_coord)
             self.current_node = subprocess.Popen('exec ' + "rosrun tf2_ros static_transform_publisher "+current_coord, stdout=subprocess.PIPE, shell = True )
             self.enable_imu=True
@@ -79,37 +82,37 @@ class ImuControl:
             # obtain a quaternion created from yaw
             odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.th)
 
-            # first, we'll publish the transform over tf
+            # # first, we'll publish the transform over tf
             self.odom_broadcaster.sendTransform(
-                (self.x, self.y, 0.),
+                (self.x, -self.y, 0.),
                 odom_quat,
                 self.current_time,
-                "base_link",
-                "odom"
+                "base_link2",
+                "odom2"
             )
 
             # next, we'll publish the odometry message over ROS
             odom = Odometry()
             odom.header.stamp = self.current_time
-            odom.header.frame_id = "odom"
+            odom.header.frame_id = "odom2"
 
             # set the position
-            odom.pose.pose = Pose(Point(self.x, self.y, 0.), Quaternion(*odom_quat))
+            odom.pose.pose = Pose(Point(self.x, -self.y, 0.), Quaternion(*odom_quat))
 
             # set the velocity
-            odom.child_frame_id = "base_link"
+            odom.child_frame_id = "base_link2"
             odom.twist.twist = Twist(Vector3(self.vx, self.vy, 0), Vector3(0, 0, self.vth))
 
             # publish the message
             self.odom_pub.publish(odom)
             self.last_time = self.current_time
             #Draw path
-            if self.x != self.past_x or self.y != self.past_y or self.th != self.past_th:
-                self.sdv_path.header.frame_id = "odom"
+            if self.x != self.past_x or -self.y != self.past_y or self.th != self.past_th:
+                self.sdv_path.header.frame_id = "odom2"
                 self.sdv_path.header.stamp = rospy.Time.now()
                 pose = PoseStamped()
                 pose.pose.position.x = self.x
-                pose.pose.position.y = self.y
+                pose.pose.position.y = -self.y
                 pose.pose.position.z = 0
 
                 quaternion = odom_quat
@@ -119,10 +122,9 @@ class ImuControl:
                 pose.pose.orientation.w = quaternion[3]
                 self.sdv_path.poses.append(pose)
                 self.past_x=self.x
-                self.past_y=self.y
+                self.past_y=-self.y
                 self.past_th=self.th
-
-            self.path_pub.publish(self.sdv_path)
+                self.path_pub.publish(self.sdv_path)
 
 
 
